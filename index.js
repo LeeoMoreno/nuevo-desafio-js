922c9a3fcb70f9bebcfbc65098211aea8e07d856
morenoleo <moreno.leeonardo@gmail.com>
2022-09-20 22:24:11 -0300

    arrays - tienda videojuegos

--- index.js
+++ index.js
@@ -1,94 +1,73 @@
+const juegos = [
+  {nombre: "Fifa 23", precio: 15000},
+  {nombre: "GTA V", precio: 10000},
+  {nombre: "Gran Turismo 7", precio: 12000},
+  {nombre: "God of War", precio: 10500}
+]
 
-function calcularPrecios() {
+let carrito = []
 
-    let productoIngresado;
-    let respuesta = "si";
-    let sumaFinalPrecios;
-    let precioChocolate = 15;
-    let precioAlfajor = 8;
-    let precioChupetin = 4;
-    let contadorChocolate = 0;
-    let contadorAlfajor = 0;
-    let contadorChupetin = 0;
-    let descuentoDel10 = 10;
-    let precioFinalChocolate;
-    let precioFinalAlfajor;
-    let precioFinalChupetin;
-    let mensajeDescuento;
-    let mensaje;
-    let cantidadTotalDeProductos;
-    let precioConDescuento;
-    let precioFinal;
-  
-  
-    alert("Bienvenidos la kiosco de Leo!")
-  
-    while (respuesta == "si") {
-      productoIngresado = prompt(`Nuestros productos son:
-      Chocolate: $15  
-      Alfajor: $8
-      Chupetin: $4
-      Que desea comprar?`);
-      while (productoIngresado != "chocolate" && productoIngresado != "alfajor" && productoIngresado != "chupetin") {
-        productoIngresado = prompt("Por favor introdusca uno de los productos disponibles", "Chocolate Alfajor o Chupetin");
-      }
-  
- 
-      switch (productoIngresado) {
-        case "chocolate":
-          contadorChocolate++;
+let pregunta = prompt ("Bienvenido, desea comprar un juego de PS4 ?")
+
+while (pregunta != "si" && pregunta != "no"){
+  alert ("Por favor, escriba si o no para continuar")
+  pregunta = prompt ("Bienvenido, desea comprar un juego de PS4 ?");
+}
+
+if (pregunta == "si"){
+  alert ("Lista de juegos disponibles:")
+  let listaDeJuegos = juegos.map((juego) => juego.nombre + " $" + juego.precio);
+  alert (listaDeJuegos.join(" - "));
+} else if (pregunta == "no"){
+  alert ("Gracias por ingresar a la tienda de Videojuegos de Leo");
+}
+
+while (pregunta != "no"){
+  let juego = prompt ("Agregue un juego a su carrito");
+  let precio = 0;
+
+  if (juego == "fifa 23"
+  || juego == "gta v"
+  || juego == "gran turismo 7"
+  || juego == "god of war"){
+    switch (juego){
+      case "fifa 23":
+        precio = 15000;
+        break;
+      case "god of war":
+        precio = 10500;
+        break
+      case "gta v":
+        precio = 10000;
+        break
+      case "gran turismo 7":
+        precio = 12000;
+        break;
+
+        default:
           break;
-  
-        case "alfajor":
-          contadorAlfajor++;
-          break;
-  
-        case "chupetin":
-          contadorChupetin++;
-          break;
-  
-      }
-      alert('Al comprar 5 o mas de un mismo producto, se le hara un 10% de descuento!');
-      respuesta = prompt("¿Desea seguir comprando?");
-      while (respuesta != "si" && respuesta != "no") {
-        respuesta = prompt('Por favor, ingrese solamente si o no');
-      }
-  
     }
-  
-  
-    precioFinalChocolate = precioChocolate * contadorChocolate;
-    precioFinalAlfajor = precioAlfajor * contadorAlfajor;
-    precioFinalChupetin = precioChupetin * contadorChupetin;
-  
-    sumaFinalPrecios = precioFinalChocolate + precioFinalAlfajor + precioFinalChupetin;
-  
-    cantidadTotalDeProductos = contadorChocolate + contadorAlfajor + contadorChupetin;
-  
-  
-    if (cantidadTotalDeProductos >= 5) {
-      mensajeDescuento = 'Felicidades, por su compra obtuvo un 10% de descuento';
-      precioConDescuento = sumaFinalPrecios/descuentoDel10;
-    }
-    else if (cantidadTotalDeProductos == 4) {
-      mensajeDescuento = 'Compre 5 o mas unidades de un mismo producto para obtener un 10% de descuento';
-      precioConDescuento=0;
-    } else {
-      mensajeDescuento = 'No tiene descuento';
-      precioConDescuento=0;
-    }
-  
-  
-    precioFinal = sumaFinalPrecios - precioConDescuento;
-  
-    //MENSAJE FINAL
-    mensaje = `Usted compró ${contadorChocolate} unidad/es de Chocolates, <br>
-    ${contadorAlfajor} unidad/es de Alfajors,<br>
-    ${contadorChupetin} unidad/es de Chupetins,<br>
-    ${mensajeDescuento} El precio final a pagar es de $${precioFinal}`;
-  
-    document.write(mensaje);
-  
+
+    let cantidad = parseInt (prompt ("Cuantas unidades quiere?"))
+
+    carrito.push({juego, cantidad, precio})
+    console.log(carrito);
+  }else {
+    alert ("No esta disponible")
   }
-  
-  calcularPrecios();
+
+  pregunta = prompt ("Quiere agregar otro juego a su compra?")
+
+  while (pregunta === "no"){
+    alert("Gracias por su compra, el monto total a pagar es:")
+    carrito.forEach(carritoTotal =>{
+      alert(`Juego: ${carritoTotal.juego}, Cantidad: ${carritoTotal.cantidad},
+      Precio: $ ${carritoTotal.precio}
+      , Subtotal Producto: $ ${carritoTotal.cantidad * carritoTotal.precio}`)
+    })
+    break
+  }
+}
+
+const total = carrito.reduce((total, juego) => total + juego.precio * juego.cantidad,0)
+alert(`El monto total de su compra es: $ ${total}`);
